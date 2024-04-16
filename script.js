a115249a6b2914cc3008bbdd48917facf8d97269
Tyler <224tpalm@frhsd.com>
2024-04-16 11:08:06 -0400

    Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var yourName = prompt("Enter your name: ");
+var occupation = prompt("Enter your favorite summer spot: ");
+var salary = prompt("Enter what you do at this place (past tense verb): ");
+var yourEXP = prompt("Enter the date in which you went to this place: ");
+var verb = prompt("Who did you go with? ");
+var pluralNoun = prompt("what was the weather? (adjective) ");
+
+document.write(yourName + " just got off a school, and for the first day of summer went to the " + occupation + ", and " + salary + " all day. " + yourEXP + " is when you went and had so much fun, " + " enjoying time with " + verb + " on this " + pluralNoun + " day.");
